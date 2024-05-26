4db1e58e2c12a9a90ca9ffb58e3e22b91213429b
Tjwever <timjwever@gmail.com>
2024-05-26 09:53:12 -0400

    animations added to fighter

--- Characters/Allies/Fighter.gd
+++ Characters/Allies/Fighter.gd
@@ -7,14 +7,17 @@ signal update_fighter_health
 @onready var healthbar = $"../CanvasLayer/PlayerSideUI/GridContainer/MarginContainer/VBoxContainer/HBoxContainer/Healthbar"
 @onready var enemy = $"../Enemy"
 @onready var timer = $Timer
+@onready var animation_player = $AnimationPlayer
 
-const BASE_WAIT_TIME = 1.0
+const BASE_WAIT_TIME = 1
 
 var max_health := 400
 var current_health := 400
 var defense := 3
-var attack_damge := 50
-var speed := 10.0
+var attack_damge := 150
+var speed := 9.0
+
+var speed_calculation: float = float(BASE_WAIT_TIME / float(speed / 10.0))
 
 func _ready():
 	healthbar.init_health(current_health)
@@ -22,7 +25,8 @@ func _ready():
 	timer.start()
 
 func set_attack_timer():
-	timer.wait_time = BASE_WAIT_TIME / (speed / 10.0)
+	timer.wait_time = speed_calculation
+	print('timer wait time: ', float(speed_calculation))
 	#print("Fighter attack wait time set to: ", timer.wait_time)
 
 func calculate_damage(atk_damage, enemy_defense):
@@ -30,8 +34,10 @@ func calculate_damage(atk_damage, enemy_defense):
 
 func attack():
 	if enemy:
+		animation_player.play("quick_attack")
+		await get_tree().create_timer(0.32).timeout
 		var damage_dealt = calculate_damage(attack_damge, enemy.defense)
-		#print('Fighter deals ', damage_dealt)
+		print('Fighter deals ', damage_dealt)
 		enemy.take_damage(damage_dealt)
 
 func take_damage(damage):
