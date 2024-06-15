3d8317e6ded6d5eaa7ab9a2768375d0816d133ed
Tjwever <timjwever@gmail.com>
2024-06-14 20:51:02 -0400

    added tutorial, moved script files to proper directory

--- Scripts/Tutorial.gd
+++ Scripts/Tutorial.gd
@@ -1,9 +1,5 @@
 extends Control
 
 
-func _on_play_pressed():
+func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/MainScene.tscn")
-
-
-func _on_quit_pressed():
-	get_tree().quit()
