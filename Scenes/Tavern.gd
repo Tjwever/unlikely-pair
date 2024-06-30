dfeaa6548e12a734f9708edd5757b0d0b77b6d53
Tjwever <timjwever@gmail.com>
2024-06-29 21:29:02 -0400

    created tavern, and added UI prompt for battle

---
+++ Scenes/Tavern.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var fight_prompt_ui = $Player/Camera2D/FightPromptUI
+@onready var player = $Player
+
+func _on_yes_pressed():
+	get_tree().change_scene_to_file("res://Scenes/MainScene.tscn")
+
+
+func _on_no_pressed():
+	fight_prompt_ui.visible = false
+	player.can_move = true
